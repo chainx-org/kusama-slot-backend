592068bf4e6429c2c589958017f9c30eb2f42b77
吴wbh1328551759 <1328551759@qq.com>
2021-05-27 14:54:05 +0800

    feat: get user and pass from .env

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,7 +1,10 @@
+const dotenv = require('dotenv')
+dotenv.config()
+const CLIENT = JSON.parse(process.env.CLIENT!)
 
 // @ts-ignore
 const mongoose = require('mongoose');
-const DB_URL: string = "mongodb://admin:password@a26cd83db9d0248dfa68340ca17fc4f6-1763314211.us-east-2.elb.amazonaws.com:27017/kusama-slot?authSource=admin";
+const DB_URL: string = `mongodb://${CLIENT.user}:${CLIENT.pass}@a26cd83db9d0248dfa68340ca17fc4f6-1763314211.us-east-2.elb.amazonaws.com:27017/kusama-slot?authSource=admin`;
 // mongoose.connect(DB_URL);
 // 根据警告的提示信息进行配置
 mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
