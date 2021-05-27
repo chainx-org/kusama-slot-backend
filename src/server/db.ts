f513eb4cceabdfb81eb155cc4a268fc2d99cdd8d
吴wbh1328551759 <1328551759@qq.com>
2021-05-27 14:59:46 +0800

    fix: delete useless code and fix bug

--- src/server/db.ts
+++ src/server/db.ts
@@ -5,6 +5,7 @@ const CLIENT = JSON.parse(process.env.CLIENT!)
 // @ts-ignore
 const mongoose = require('mongoose');
 const DB_URL: string = `mongodb://${CLIENT.user}:${CLIENT.pass}@a26cd83db9d0248dfa68340ca17fc4f6-1763314211.us-east-2.elb.amazonaws.com:27017/kusama-slot?authSource=admin`;
+
 // mongoose.connect(DB_URL);
 // 根据警告的提示信息进行配置
 mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
