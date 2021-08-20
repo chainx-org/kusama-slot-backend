b911d481cb1bf0119fff25d53e49135c2a1f1899
吴wbh1328551759 <1328551759@qq.com>
2021-08-20 12:16:31 +0800

    chore: update envfrom in prod deploy

--- src/server/db.ts
+++ src/server/db.ts
@@ -2,10 +2,8 @@
 // @ts-ignore
 const mongoose = require('mongoose');
 const DB_URL: string = process.env.KUSAMASLOT_URL!;
-// mongoose.connect(DB_URL);
-// 根据警告的提示信息进行配置
+
 mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
-// mongoose.auth('admin', 'XqQB3QR.j9TtEYLpZsUhF.-_')
 
 mongoose.connection.on('connected', () => {
   console.log('数据库连接成功')
